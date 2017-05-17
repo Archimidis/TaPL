3fa91e79e81ea025226ba29df875dcbb778a54c3
Nikolas Vourlakis <nvourlakis@gmail.com>
2017-05-18 01:38:15 +0300

    (untypedlambda) Add a REPL executable

--- untypedlambda/app/Main.hs
+++ untypedlambda/app/Main.hs
@@ -1,2 +1,31 @@
+module Main
+  ( main
+  ) where
+
+import Control.Applicative ((<$>))
+import System.Console.Haskeline
+
+import Text.Parsec (ParseError)
+
+import qualified Core as C
+import qualified Parser as P
+import Syntax (Term(..))
+
 main :: IO ()
-main = print "parseUntypedLambda"
+main = runInputT defaultSettings loop
+
+loop :: InputT IO ()
+loop = do
+  input <- getInputLine "lambda> "
+  case input of
+    Nothing -> return ()
+    Just "quit" -> return ()
+    Just "q" -> return ()
+    Just s -> do
+      case readEval s of
+        Left msg -> outputStrLn $ show msg
+        Right expr -> outputStrLn $ show expr
+      loop
+
+readEval :: String -> Either ParseError Term
+readEval input = C.eval1 <$> P.parseUntypedLambda input
