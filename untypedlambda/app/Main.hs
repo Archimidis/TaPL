43084109e5f1a9bb521c60a05b59614198bff8b1
Nikolas Vourlakis <nvourlakis@gmail.com>
2017-05-19 00:14:44 +0300

    (untypedlambda) Add some ready examples to execute in terminal

--- untypedlambda/app/Main.hs
+++ untypedlambda/app/Main.hs
@@ -21,11 +21,32 @@ loop = do
     Nothing -> return ()
     Just "quit" -> return ()
     Just "q" -> return ()
+    Just "test" -> showResult (cSucc ++ " " ++ c0) >> loop
+    Just "c0" -> showResult c0 >> loop
+    Just "c1" -> showResult c1 >> loop
     Just s -> do
       case readEval s of
         Left msg -> outputStrLn $ show msg
         Right expr -> outputStrLn $ show expr
       loop
 
+showResult s =
+  case readEval s of
+    Left msg -> outputStrLn $ show msg
+    Right expr -> outputStrLn $ show expr
+
 readEval :: String -> Either ParseError Term
 readEval input = C.eval1 <$> P.parseUntypedLambda input
+
+{- Church Encoding for numbers 0 and 1 and operations plus and succ -}
+c0 :: String
+c0 = "(λf.λx.x)"
+
+c1 :: String
+c1 = "(λf.λx.f x)"
+
+cPlus :: String
+cPlus = "(λm.λn.λf.λx.m f (n f x))"
+
+cSucc :: String
+cSucc = "(λn.λf.λx.f (n f x))"
