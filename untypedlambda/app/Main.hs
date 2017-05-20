17ba45a84992a3f31710a476ef680df87ab218fa
Nikolas Vourlakis <nvourlakis@gmail.com>
2017-05-21 00:31:34 +0300

    (untypedlambda) Use pretty printer in executable

--- untypedlambda/app/Main.hs
+++ untypedlambda/app/Main.hs
@@ -3,13 +3,12 @@ module Main
   ) where
 
 import Control.Applicative ((<$>))
+import Control.Monad.IO.Class (MonadIO(..))
 import System.Console.Haskeline
-
-import Text.Parsec (ParseError)
 
 import qualified Core as C
 import qualified Parser as P
-import Syntax (Term(..))
+import Pretty (renderExpression)
 
 main :: IO ()
 main = runInputT defaultSettings loop
@@ -19,31 +18,35 @@ loop = do
   input <- getInputLine "lambda> "
   case input of
     Nothing -> return ()
+    Just "" -> loop
     Just "quit" -> return ()
     Just "q" -> return ()
-    Just "test" -> showResult (cSucc ++ " " ++ c0) >> loop
-    Just "c0" -> showResult c0 >> loop
-    Just "c1" -> showResult c1 >> loop
-    Just s -> do
-      case readEval s of
+    Just "c0" -> readEvalPrint c0 >> loop
+    Just "c1" -> readEvalPrint c1 >> loop
+    Just "c2" -> readEvalPrint c2 >> loop
+    Just "succ0" -> readEvalPrint (cSucc ++ " " ++ c0) >> loop
+    Just "succ1" -> readEvalPrint (cSucc ++ " " ++ c1) >> loop
+    Just "plus" -> readEvalPrint (cPlus ++ " " ++ c0 ++ " " ++ c1) >> loop
+    Just s -> readEvalPrint s >> loop
+
+readEvalPrint :: MonadIO m => String -> InputT m ()
+readEvalPrint input =
+  showResult $ C.eval1 <$> P.parseUntypedLambda input
+  where
+    showResult x =
+      case x of
         Left msg -> outputStrLn $ show msg
-        Right expr -> outputStrLn $ show expr
-      loop
+        Right expr -> outputStrLn $ renderExpression expr
 
-showResult s =
-  case readEval s of
-    Left msg -> outputStrLn $ show msg
-    Right expr -> outputStrLn $ show expr
-
-readEval :: String -> Either ParseError Term
-readEval input = C.eval1 <$> P.parseUntypedLambda input
-
-{- Church Encoding for numbers 0 and 1 and operations plus and succ -}
+{- Church Encoding for numbers {0, 1, 2} and operations plus and succ -}
 c0 :: String
 c0 = "(λf.λx.x)"
 
 c1 :: String
 c1 = "(λf.λx.f x)"
+
+c2 :: String
+c2 = "(λf.λx.f (f x))"
 
 cPlus :: String
 cPlus = "(λm.λn.λf.λx.m f (n f x))"
